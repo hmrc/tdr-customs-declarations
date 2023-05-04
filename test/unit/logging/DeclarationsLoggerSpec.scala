ca7a9cb9a96b28cac29c4bd6423be2428762c38e
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-05-04 14:00:01 +0100

    testingfix

--- test/unit/logging/DeclarationsLoggerSpec.scala
+++ test/unit/logging/DeclarationsLoggerSpec.scala
@@ -19,7 +19,6 @@ package unit.logging
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 import org.scalatestplus.mockito.MockitoSugar
-import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.mvc.AnyContentAsXml
 import play.api.test.FakeRequest
 import uk.gov.hmrc.customs.api.common.logging.CdsLogger
@@ -27,19 +26,14 @@ import uk.gov.hmrc.customs.declaration.logging.DeclarationsLogger
 import uk.gov.hmrc.customs.declaration.model.actionbuilders.ActionBuilderModelHelper._
 import uk.gov.hmrc.customs.declaration.model.actionbuilders.{ApiVersionRequest, AuthorisedRequest}
 import uk.gov.hmrc.customs.declaration.model.{Csp, VersionOne}
-import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 import util.CustomsDeclarationsMetricsTestData.EventStart
 import util.MockitoPassByNameHelper.PassByNameVerifier
 import util.TestData._
 
-import scala.reflect.ClassTag
-
-class DeclarationsLoggerSpec extends AnyWordSpecLike with MockitoSugar with Matchers with GuiceOneAppPerSuite{
+ class DeclarationsLoggerSpec extends AnyWordSpecLike with MockitoSugar with Matchers{
 
   trait SetUp {
-    def real[T: ClassTag]: T = app.injector.instanceOf[T]
     val mockCdsLogger: CdsLogger = mock[CdsLogger]
-    val cdsLogger: CdsLogger = new CdsLogger(real[ServicesConfig])
     val logger = new DeclarationsLogger(mockCdsLogger)
     implicit val implicitVpr: AuthorisedRequest[AnyContentAsXml] = ApiVersionRequest(conversationId, EventStart, VersionOne, FakeRequest()
       .withXmlBody(TestXmlPayload).withHeaders("Content-Type" -> "Some-Content-Type"))
