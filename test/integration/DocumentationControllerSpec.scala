d19ad716050bc99903546efe855b93aa2a8e5aff
Tawanda Siyamachira <62283841+TSiyamachiraQA@users.noreply.github.com>
2024-06-18 11:22:42 +0100

    DCWL-2329 Amend logger name in test config

--- test/integration/DocumentationControllerSpec.scala
+++ test/integration/DocumentationControllerSpec.scala
@@ -39,7 +39,7 @@ class DocumentationControllerSpec extends IntegrationTestSpec with MockitoSugar 
     configure(
       Map(
         "play.http.router" -> "definition.Routes",
-        "application.logger.name" -> "customs-api-common",
+        "application.logger.name" -> "tdr-customs-declarations",
         "appName" -> "tdr-customs-declarations",
         "appUrl" -> "http://customs-wco-declaration.service",
         "auditing.enabled" -> false,
